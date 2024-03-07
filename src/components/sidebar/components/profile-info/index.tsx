e8bbfe6c5831d043c2d34d2ad6757d600deef2dd
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-03-07 22:33:13 +0400

    changed avatar animation

--- src/components/sidebar/components/profile-info/index.tsx
+++ src/components/sidebar/components/profile-info/index.tsx
@@ -4,13 +4,15 @@ import Link from "next/link";
 function ProfileInfo() {
   return (
     <header className="flex items-center gap-4 flex-col mb-5">
-      <Image
-        src="https://bootstrapmade.com/demo/templates/iPortfolio/assets/img/profile-img.jpg"
-        alt="Logo"
-        width={200}
-        height={200}
-        className="rounded-full border-8 border-primary"
-      />
+      <div className="rounded-full border-8 border-primary overflow-hidden group hover:scale-90 transition-all duration-200 cursor-none">
+        <Image
+          src="https://bootstrapmade.com/demo/templates/iPortfolio/assets/img/profile-img.jpg"
+          alt="Logo"
+          width={200}
+          height={200}
+          className="group-hover:scale-125 transition-all duration-200"
+        />
+      </div>
       <Link href={"#"}>
         <h1 className="font-bold text-2xl">Nijat Niyazov</h1>
       </Link>
