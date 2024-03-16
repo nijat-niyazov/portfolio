c263defaff8922cef35c1aa62d9ba1fccc00a6f3
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-03-16 16:33:12 +0400

    testin sidebar

--- src/components/mobilesidebar/index.tsx
+++ src/components/mobilesidebar/index.tsx
@@ -6,6 +6,31 @@ import { useState } from "react";
 import Backdrop from "./backdrop";
 import ToggleButton from "./toggle-btn";
 
+// const MobileSidebar = () => {
+//   const [isOpened, setIsOpened] = useState(false);
+
+//   const handleToggle = () => setIsOpened(!isOpened);
+
+//   return (
+//     <div className="fixed w-full h-screen md:hidden z-50">
+//       <Backdrop handleToggle={handleToggle} isOpened={isOpened} />
+
+//       <ToggleButton handleToggle={handleToggle} isOpened={isOpened} />
+//       <div
+//         className={cn("w-9/12 absolute inset-0 h-screen transition-all ease-in-out duration-500 -translate-x-full", {
+//           "translate-x-0": isOpened,
+//         })}
+//       >
+//         <div className="bg-primary h-screen  pt-20">
+//           <SideBar handleOnNavigation={handleToggle} />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MobileSidebar;
+
 const MobileSidebar = () => {
   const [isOpened, setIsOpened] = useState(false);
 
@@ -16,11 +41,7 @@ const MobileSidebar = () => {
       <Backdrop handleToggle={handleToggle} isOpened={isOpened} />
 
       <ToggleButton handleToggle={handleToggle} isOpened={isOpened} />
-      <div
-        className={cn("w-9/12 absolute inset-0 h-screen transition-all ease-in-out duration-500 -translate-x-full", {
-          "translate-x-0": isOpened,
-        })}
-      >
+      <div className={cn("w-9/12 absolute inset-0 h-screen transition-all ease-in-out duration-500 menu", { expanded: isOpened })}>
         <div className="bg-primary h-screen  pt-20">
           <SideBar handleOnNavigation={handleToggle} />
         </div>
@@ -28,25 +49,4 @@ const MobileSidebar = () => {
     </div>
   );
 };
-
 export default MobileSidebar;
-
-// const MobileSidebar = () => {
-//   const [isOpened, setIsOpened] = useState(false);
-
-//   const handleToggle = () => setIsOpened(!isOpened);
-
-//   return (
-//     <div className="fixed w-full h-screen md:hidden z-50">
-//       <Backdrop handleToggle={handleToggle} isOpened={isOpened} />
-
-//       <div className={cn("w-9/12 absolute inset-0 h-screen transition-all ease-in-out duration-500 menu", { expanded: isOpened })}>
-//         <ToggleButton handleToggle={handleToggle} isOpened={isOpened} />
-
-//         <div className="bg-primary h-screen  pt-20">
-//           <SideBar handleOnNavigation={handleToggle} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
