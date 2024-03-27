a9723df26f4594607435704c2c35f68c6bc1e4b6
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-03-27 23:28:08 +0400

    behavior changed

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,10 +3,10 @@ import { Footer, MenuNavigation, ProfileInfo, SocialIcons } from "./components";
 const SideBar = ({ handleOnNavigation }: { handleOnNavigation?: () => void }) => {
   return (
     <div className="h-full md:h-screen bg-black text-white p-2 top-0 z-50 md:p-10  md:sticky grid ">
-      <div>
+      <header>
         <ProfileInfo />
         <SocialIcons />
-      </div>
+      </header>
 
       <MenuNavigation handleOnNavigation={handleOnNavigation} />
 
