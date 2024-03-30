b1c875084f0c617f81dab646cbf06bb6e81ab5e5
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-03-31 01:20:35 +0400

    changed img

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,7 +3,7 @@ import { Footer, MenuNavigation, ProfileInfo, SocialIcons } from "./components";
 const SideBar = ({ handleOnNavigation }: { handleOnNavigation?: () => void }) => {
   return (
     <div className="h-full md:h-screen bg-secondary text-white p-2 top-0 z-50 md:p-10  md:sticky grid ">
-      <header>
+      <header className="md:mb-3">
         <ProfileInfo />
         <SocialIcons />
       </header>
