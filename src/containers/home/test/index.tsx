c30d6f6d7c06c93af49f6e91a9b1e5e0b4e2bcea
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-03-30 13:59:26 +0400

    made changes for mobile v

---
+++ src/containers/home/test/index.tsx
@@ -0,0 +1,10 @@
+const Test = () => {
+  return (
+    <div className="bg-ios">
+      <div className="bg-ios__back"></div>
+      <h1 className="text-3xl text-white">Content</h1>
+    </div>
+  );
+};
+
+export default Test;
