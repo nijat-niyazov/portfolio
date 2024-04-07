a8138ddb5909a9a9959a3de21170b863e878ee99
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2024-04-08 00:38:37 +0400

    updated seo data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nijat Niyazov - Portfolio",
-  description: "Front end developer portfolio website. Nijat Niyazov. React, Next.js, Tailwind CSS, Framer Motion, and more.",
+  title: "Nijat Niyazov - Front end developer",
+  description: "Main skills - React, Next.js, Tailwind CSS, Framer Motion, and more.",
+  keywords: ["Front end developer", "React", "Next.js", "Tailwind CSS", "Framer Motion", "Next.js App router", "app router", "Next.js 14"],
   openGraph: {
     images: [
       "https://scontent.fgyd12-1.fna.fbcdn.net/v/t39.30808-6/337039068_239638081859927_3890019577032024805_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ojmjZXd-Pk8Ab5V8kNf&_nc_ht=scontent.fgyd12-1.fna&oh=00_AfDDoQOtgMPINc4QsfqfZb25txq--I-EzaXwBANclgsVKQ&oe=6618E335",
